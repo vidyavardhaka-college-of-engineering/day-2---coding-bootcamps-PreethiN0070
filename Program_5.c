2e0ee2f2378cd450b10f9e7fd7fc47ca8415e79d
PreethiN0070 <Preethinarayan2003@gmail.com>
2021-02-16 12:02:27 +0000

    swaping two numbers

--- Program_5.c
+++ Program_5.c
@@ -11,12 +11,14 @@ int main()
 {
     int a, b, temp;
     scanf("%d%d", &a, &b);
-
+    printf("the values of a and b befor swaping is:%d %d\n",a,b);
     //Write your code here
+    temp = a;
+    a = b;
+    b = temp;
     
-    
-
+   printf("the values of a and b after swaping is:%d %d\n",a,b);
   
-    printf("%d %d\n", a, b);
+    //printf("%d %d\n", a, b);
     return 0;
 }
